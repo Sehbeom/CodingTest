48bf96147f375ba360342e7790f98eb6d325ad8f
Sehbeom <j3beom@gmail.com>
2022-08-05 19:16:31 +0900

    :sparkles: 220805 크레인 인형뽑기 게임

--- Test.py
+++ Test.py
@@ -1,4 +1,6 @@
-a = 'one'
-b = 'one234'
-b = b.replace(a, '1')
-print(b)
+board = [[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [
+    0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]]
+for b in board:
+    print(b)
+cols = list(map(lambda x: list(filter(lambda y: y > 0, x)), zip(*board)))
+print(cols)
