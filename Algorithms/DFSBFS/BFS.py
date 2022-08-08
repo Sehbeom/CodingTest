8b0d17f8b57cc6aefa835034e89df7b32887de8a
Sehbeom <j3beom@gmail.com>
2022-08-08 17:42:25 +0900

    :zap: DFS&BFS 수정 및 내용 추가

--- Algorithms/DFSBFS/BFS.py
+++ Algorithms/DFSBFS/BFS.py
@@ -14,18 +14,6 @@ graph = [
 
 visited = [False]*len(graph)
 
-# def BFS(graph, start, visited):
-#     deq = deque([start])
-#     visited[start] = True
-
-#     while deq:
-#         v = deq.popleft()
-#         print(v, end=' ')
-#         for i in graph[v]:
-#             if not visited[i]:
-#                 visited[i] = True
-#                 deq.append(i)
-
 def BFS(graph, start, visited):
     queue = deque([start])
     visited[start] = True
@@ -37,20 +25,5 @@ def BFS(graph, start, visited):
             if not visited[i]:
                 visited[i] = True
                 queue.append(i)
-
-# def BFS(graph, start, visited):
-#     # 시작 노드를 큐에다가 먼저 삽입(삽입할 때 파이썬 리스트[]로 감싸주기)
-#     queue = deque([start])
-#     # 시작 노드를 방문 처리
-#     visited[start] = True
-
-#     # 큐에서 노드를 pop하고 그 노드의 인접노드들을 탐색. 단, 큐가 빌(False)때 까지
-#     while queue:
-#         v = queue.popleft()
-#         print(v, end=' ')
-#         for i in graph[v]:
-#             if not visited[i]:
-#                 visited[i] = True
-#                 queue.append(i)
 
 BFS(graph, 1, visited)
--- Algorithms/DFSBFS/DFS.py
+++ Algorithms/DFSBFS/DFS.py
@@ -10,15 +10,6 @@ graph = [
     [1, 7]
 ]
 
-visited = [False]*len(graph)
-
-# def DFS(graph, v, visited):
-#     visited[v] = True
-#     print(v, end=' ')
-#     for i in graph[v]:
-#         if not visited[i]:
-#             DFS(graph, i, visited)
-
 def DFS(graph, start, visited):
     visited[start] = True
     print(start, end=' ')
@@ -26,4 +17,25 @@ def DFS(graph, start, visited):
         if not visited[i]:
             DFS(graph, i, visited)
 
-DFS(graph, 1, visited)
+def DFS_Stack(graph, start, visited):
+    stack = [start]
+    temp = []
+
+    while stack:
+        v = stack.pop()
+        if not visited[v]:
+            visited[v] = True
+            print(v, end=" ")
+
+        for i in graph[v]:
+            if not visited[i]:
+                temp.append(i)
+
+        temp.reverse()
+        stack.extend(temp)
+        temp.clear()
+
+
+DFS(graph, 1, [False]*len(graph))
+print("\n")
+DFS_Stack(graph, 1, [False]*len(graph))
