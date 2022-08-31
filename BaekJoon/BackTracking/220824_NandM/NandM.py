de3f80bcb63015cb6e6b0883aabd0fbe6a167ca4
Sehbeom <j3beom@gmail.com>
2022-09-01 01:57:22 +0900

    :zap: 220831 백준 15649번 N과 M 1 재귀버전

--- BaekJoon/BackTracking/220824_NandM/NandM.py
+++ BaekJoon/BackTracking/220824_NandM/NandM.py
@@ -26,6 +26,20 @@ def findSequence(stack):
                 flag = False
 
 
+def findSequence2(stack):
+    ptr = 0
+    while ptr < N:
+        ptr += 1
+        if not ptr in stack:
+            stack.append(ptr)
+            if len(stack) == M:
+                print(*stack)
+                stack.pop()
+            else:
+                findSequence2(stack)
+                stack.pop()
+
+
 N, M = map(int, input().split())
 
-findSequence([1])
+findSequence2([])
