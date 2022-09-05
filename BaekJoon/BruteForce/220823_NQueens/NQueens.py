932b551d54a891e5e41cb927f826b7a93c41ed91
Sehbeom <j3beom@gmail.com>
2022-09-06 00:26:09 +0900

    :sparkles: 220905 백준 1759번 암호 만들기

--- BaekJoon/BruteForce/220823_NQueens/NQueens.py
+++ BaekJoon/BruteForce/220823_NQueens/NQueens.py
@@ -57,6 +57,111 @@ def putQueens(board, startPos):
                     putQueens(curBoard, startPos + N)
 
 
+def canPutQueens(curStack, curPos):
+    if not curStack:
+        return True
+    else:
+        stack = curStack[:]
+        isPossible = True
+        horizonStart = curPos - ((curPos % N) - 1) if not (curPos %
+                                                           N == 0) else curPos - N + 1
+        while stack:
+            s = stack.pop()
+            if s % N == curPos % N:
+                isPossible = False
+                break
+
+            diag_rb = s
+            diag_lb = s
+
+            while True:
+                if diag_rb % N != 0:
+                    diag_rb += (N+1)
+
+                if diag_lb % N != 1:
+                    diag_lb += (N-1)
+
+                if (diag_rb == curPos) or (diag_lb == curPos):
+                    isPossible = False
+                    break
+
+                if (horizonStart <= diag_rb < horizonStart + N) or (horizonStart <= diag_lb < horizonStart + N):
+                    break
+
+                if (diag_rb % N == 0) and (diag_lb % N == 1):
+                    break
+
+        return isPossible
+
+
+def putQueens3(stack, startPos):
+    global answer
+
+    for i in range(startPos, startPos+N):
+        if canPutQueens(stack, i):
+            stack.append(i)
+
+            if (startPos + N) == (N*N + 1):
+                answer += 1
+                stack.pop()
+                break
+
+            else:
+                putQueens3(stack, startPos + N)
+                stack.pop()
+
+
+def putQueens4(stack, startPos):
+    global answer
+
+    for i in range(startPos, startPos+N):
+        isPossible = True
+        if not stack:
+            isPossible = True
+        else:
+            curStack = stack[:]
+            horizonStart = i - ((i % N) - 1) if not (i % N == 0) else i - N + 1
+
+            while curStack:
+                s = curStack.pop()
+
+                if s % N == i % N:
+                    isPossible = False
+                    break
+
+                diag_rb = s
+                diag_lb = s
+
+                while True:
+                    if diag_rb % N != 0:
+                        diag_rb += (N+1)
+
+                    if diag_lb % N != 1:
+                        diag_lb += (N-1)
+
+                    if (diag_rb == i) or (diag_lb == i):
+                        isPossible = False
+                        break
+
+                    if (horizonStart <= diag_rb < horizonStart + N) or (horizonStart <= diag_lb < horizonStart + N):
+                        break
+
+                    if (diag_rb % N == 0) and (diag_lb % N == 1):
+                        break
+
+        if isPossible:
+            stack.append(i)
+
+            if (startPos + N) == (N*N + 1):
+                answer += 1
+                stack.pop()
+                break
+
+            else:
+                putQueens4(stack, startPos + N)
+                stack.pop()
+
+
 def putQueens2(board, startPos):
     global answer
     for i in range(startPos, startPos+N):
@@ -97,14 +202,16 @@ def putQueens2(board, startPos):
 
             else:
                 if True in curBoard[startPos+N:startPos+2*N]:
-                    putQueens(curBoard, startPos + N)
+                    putQueens2(curBoard, startPos + N)
 
 
 N = int(input())
-# startTime = time.time()
+startTime = time.time()
 board = [True for i in range(N*N + 1)]
 
 answer = 0
-putQueens(board, 1)
+putQueens4([], 1)
+# putQueens2(board, 1)
+# print(canPutQueens([1, 8, 15, 17], 21))
 print(answer)
-# print("time : ", time.time() - startTime)
+print("time : ", time.time() - startTime)
