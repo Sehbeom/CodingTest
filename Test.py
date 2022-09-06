28eed3dfc541467f1dfdc5a45b231ca97af147c2
Sehbeom <j3beom@gmail.com>
2022-09-06 16:15:27 +0900

    :sparkles: 220906 백준 10815 숫자 카드

--- Test.py
+++ Test.py
@@ -1,5 +1,15 @@
-a = 10
-b = 20
-c = 30
-print(a, b, end="0")
-print(c)
+from collections import deque
+
+# queue1 = deque( < 리스트 > )
+queue1 = deque([1, 2, 3])
+
+print(queue1)
+print(queue1.popleft())
+print(queue1)
+# 1
+# [2, 3]
+
+print(queue1.pop())
+print(queue1)
+# 3
+# [2]
