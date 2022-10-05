394754c7050a49a4bc6b048186684a4859911517
Sehbeom <j3beom@gmail.com>
2022-10-05 11:48:45 +0900

    :sparkles: 221005 백준 2293번 동전1

--- BaekJoon/DynamicProgramming/221004_BuyingCard/BuyingCard.py
+++ BaekJoon/DynamicProgramming/221004_BuyingCard/BuyingCard.py
@@ -2,46 +2,50 @@
 # 백준 / 11052번 카드 구매하기
 
 # 내가 푼 코드
-N = int(input())
-cards = list(map(int, input().split()))
+# N = int(input())
+# cards = list(map(int, input().split()))
 
-answer = 0
-maxResultList = [0] * N
-maxResultList[0] = cards[0]
+# answer = 0
+# DN = [0] * N
+# DN[0] = cards[0]
 
-for i in range(1, len(cards)):
-    start = -1
-    end = i
-    newOne = 0
-    while start < end:
-        start += 1
-        end -= 1
-        if start == end:
-            if newOne < (maxResultList[start] * 2):
-                newOne = maxResultList[start] * 2
-        else:
-            if newOne < (maxResultList[start] + maxResultList[end]):
-                newOne = maxResultList[start] + maxResultList[end]
+# for i in range(1, len(cards)):
+#     start = -1
+#     end = i
+#     newOne = 0
+#     while start < end:
+#         start += 1
+#         end -= 1
+#         if start == end:
+#             if newOne < (DN[start] * 2):
+#                 newOne = DN[start] * 2
+#         else:
+#             if newOne < (DN[start] + DN[end]):
+#                 newOne = DN[start] + DN[end]
 
-    if newOne < cards[i]:
-        maxResultList[i] = cards[i]
-        answer = cards[i]
-    else:
-        maxResultList[i] = newOne
-        answer = newOne
+#     if newOne < cards[i]:
+#         DN[i] = cards[i]
+#         answer = cards[i]
+#     else:
+#         DN[i] = newOne
+#         answer = newOne
 
 
-print(answer)
+# print(answer)
 
 # 다른 사람의 코드
 # 방식은 동일하나 더 쉽게 풀 수 있음
-n = int(input())
-d = [0] * (n + 1)
-p = [0] + list(map(int, input().split()))
-d[1] = p[1]
-for i in range(2, n + 1):
-    print(d)
-    for j in range(1, i + 1):
-        if d[i] < d[i - j] + p[j]:
-            d[i] = d[i - j] + p[j]
-print(d[n])
+N = int(input())
+cards = [0]
+cards += list(map(int, input().split()))
+
+DN = [0] * (N+1)
+DN[1] = cards[1]
+DN[2] = max(cards[2], cards[1]*2)
+
+for i in range(3, N+1):
+    DN[i] = cards[i]
+    for j in range(1, i//2 + 1):
+        DN[i] = max(DN[i], DN[j] + DN[i-j])
+
+print(DN[N])
